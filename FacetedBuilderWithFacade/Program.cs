f67c245bb5df000984cd681675d1d095b15c6508
AndriiLetskaliuk <andrewletskalyuk@gmail.com>
2022-11-10 11:36:26 +0200

    factories

---
+++ FacetedBuilderWithFacade/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using static System.Console;
+
+namespace FacetedBuilderWithFacade
+{
+    public class Person
+    {
+        //address
+        public string StreetAddress, Postcode, City;
+
+        //employment
+        public string CompanyName, Position;
+        public int AnnualIncome;
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(Postcode)}: {Postcode}, " +
+                   $"{nameof(City)}: {City}, {nameof(CompanyName)}: {CompanyName}, " +
+                   $"{nameof(Position)}: {Position}, {nameof(AnnualIncome)}: {AnnualIncome}";
+        }
+    }
+
+    public class PersonBuilder //facade
+    {
+        //reference!
+        protected Person person = new Person();
+
+        public PersonJobBuilder Works => new PersonJobBuilder(person);
+        public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+
+        public static implicit operator Person(PersonBuilder pb)
+        {
+            return pb.person;
+        }
+    }
+
+    public class PersonAddressBuilder : PersonBuilder
+    {
+        public PersonAddressBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonAddressBuilder At(string streetAddress)
+        {
+            person.StreetAddress = streetAddress;
+            return this;
+        }
+
+        public PersonAddressBuilder WithPostCode(string postcode)
+        {
+            person.Postcode = postcode;
+            return this;
+        }
+
+        public PersonAddressBuilder In(string city)
+        {
+            person.City = city;
+            return this;
+        }
+    }
+
+    public class PersonJobBuilder : PersonBuilder
+    {
+        public PersonJobBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonJobBuilder At(string companyName)
+        {
+            person.CompanyName = companyName;
+            return this;
+        }
+
+        public PersonJobBuilder AsA(string position)
+        {
+            person.Position = position;
+            return this;
+        }
+
+        public PersonJobBuilder Earning(int amount)
+        {
+            person.AnnualIncome = amount;
+            return this;
+        }
+    }
+
+    
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pb = new PersonBuilder();
+            Person person = pb
+                    .Lives.At("Volunskoi Duvisii 9")
+                      .In("Rivne city")
+                      .WithPostCode("33008")
+                    .Works.At("EPAM")
+                      .AsA("Software engineer")
+                      .Earning(5000);
+            
+            WriteLine(person);
+            WriteLine("Hello World!");
+        }
+    }
+}
