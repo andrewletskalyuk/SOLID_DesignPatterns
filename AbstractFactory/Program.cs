a03df829725c43319beb4525f4340e8c2f59a083
AndriiLetskaliuk <andrewletskalyuk@gmail.com>
2022-11-14 19:39:41 +0200

    PrototypePattern

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -50,45 +50,102 @@ namespace AbstractFactory
 
     public class HotDrinkMachine
     {
-        public enum AvailableDrink // violates open-closed
-        {
-            Coffee, Tea
-        }
+        /// <summary>
+        /// ця частина буде переписана щоб не порушувати принцип open-closed
+        /// </summary>
+        //public enum AvailableDrink // порушує принцип open-closed
+        //{
+        //    Coffee, Tea
+        //}
 
-        private protected Dictionary<AvailableDrink, IHotDrinkFactory> Factories
-            = new Dictionary<AvailableDrink, IHotDrinkFactory>();
+        //private protected Dictionary<AvailableDrink, IHotDrinkFactory> Factories
+        //    = new Dictionary<AvailableDrink, IHotDrinkFactory>();
 
+        //public HotDrinkMachine()
+        //{
+        //    foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+        //    {
+        //        var factory = (IHotDrinkFactory)Activator.CreateInstance(
+        //            Type.GetType("AbstractFactory." + Enum.GetName(typeof(AvailableDrink), drink) + "Factory")
+        //        );
+        //        Factories.Add(drink, factory);
+        //    }
+        //}
+
+        //public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+        //{
+        //    return Factories[drink].Prepare(amount);
+        //}
+        
+        
+        private List<Tuple<string, IHotDrinkFactory>> _factories = 
+            new List<Tuple<string, IHotDrinkFactory>>();
         public HotDrinkMachine()
         {
-            foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+            foreach (var t in typeof(HotDrinkMachine).Assembly.GetTypes())
             {
-                var factory = (IHotDrinkFactory)Activator.CreateInstance(
-                    Type.GetType("AbstractFactory." + Enum.GetName(typeof(AvailableDrink), drink) + "Factory")
-                );
-                Factories.Add(drink, factory);
-            }
-            //foreach (var t in typeof(HotDrinkMachine).Assembly.GetTypes())
-            //{
-            //    if (typeof(IHotDrinkFactory).IsAssignableFrom(t) && !t.IsInterface)
-            //    {
-            //        namedFactories.Add(Tuple.Create(
-            //            t.Name.Replace("Factory", string.Empty), (IHotDrinkFactory)Activator.CreateInstance(t)));
-            //    }
-            //}
+                if (typeof(IHotDrinkFactory).IsAssignableFrom(t) 
+                    && !t.IsInterface)
+                {
+                    _factories.Add(Tuple.Create(
+                        t.Name.Replace("Factory", string.Empty),
+                        (IHotDrinkFactory)Activator.CreateInstance(t)
+                        ));
+                }
+            }    
         }
 
-        public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+        public IHotDrink MakeDrink()
         {
-            return Factories[drink].Prepare(amount);
+            WriteLine("Available drinks:");
+            for (int index = 0; index < _factories.Count; index++)
+            {
+                var tuple = _factories[index];
+                WriteLine($"{index}: {tuple.Item1}");
+            }
+
+            while (true)
+            {
+                string s;
+                if ((s = ReadLine())!=null && int.TryParse(s, out int i)
+                    && i>=0
+                    && i < _factories.Count)
+                {
+                    Write("Specify amount: ");
+                    s = ReadLine();
+                    if (s!= null && int.TryParse(s, out int amount) && amount>0)
+                    {
+                        return _factories[i].Item2.Prepare(amount);
+                    }
+                }
+            }
         }
     }
+
+    public class One
+    {
+        public string _name;
+    }
+
+    public class Two
+    {
+        public string _name;
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            //var machine = new HotDrinkMachine();
+            //var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 100);
+            //drink.Consume();
+
             var machine = new HotDrinkMachine();
-            var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 100);
-            drink.Consume();
+            var drink = machine.MakeDrink();
+            WriteLine(drink);
+            //
+            (One, Two) t = (new One{_name = "title"}, new Two{_name = "title"});
+
         }
     }
 }
---
+++ HomeWorkFactory/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using static System.Console;
+
+namespace HomeWorkFactory
+{
+    public interface IPerson
+    {
+        void Create();
+    }
+
+    public class Person : IPerson
+    {
+        public int Id = 0;
+        public string Name { get; set; }
+        public void Create()
+        {
+            WriteLine($"Person {Name} was created");
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Id)}: {Id}";
+        }
+    }
+
+    public interface IPersonFactory
+    {
+        IPerson Prepare(string name);
+    }
+    public class PersonFactory : IPersonFactory
+    {
+        public List<Person> _persons = new List<Person>();
+        public int id = 0;
+        public IPerson Prepare(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                var person = new Person { Id = id++, Name = name };
+                _persons.Add(person);
+                WriteLine($"Person with name: {name} was prepared!!!");
+                return person;
+            }
+            return _persons[id];
+        }
+
+        public override string ToString()
+        {
+            if (_persons.Count != 0)
+            {
+                foreach (var person in _persons)
+                {
+                    person.ToString();
+                }
+            }
+            return null;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new PersonFactory();
+            factory.Prepare("Andrew").Create();
+            factory.Prepare("Ivan").Create();
+            foreach (var person in factory._persons)
+            {
+                WriteLine(person.ToString());
+            }
+        }
+    }
+}
