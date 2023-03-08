02c08e6578b3e70a1417fd0f04deb8bd309018f1
AndriiLetskaliuk <andrewletskalyuk@gmail.com>
2023-03-08 17:00:12 +0200

    OrchestrationExample

---
+++ OrchestrationExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OrchestrationExample.Services;
+using WorkflowCore.Interface;
+using static System.Console;
+
+namespace OrchestrationExample
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            IServiceProvider serviceProvider = ConfigureServices();
+            var host = serviceProvider.GetService<IWorkflowHost>();
+            if (host!=null)
+            {
+                host.RegisterWorkflow<SampleWorkFlow>();
+                host.Start();
+                host.StartWorkflow("Sample");
+                ReadLine();
+                host.Stop();
+            }
+        }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddLogging();
+            services.AddWorkflow();
+            services.AddTransient<LastStep>();
+            var serviceProvider = services.BuildServiceProvider();
+            return serviceProvider;
+        }
+    }
+}
---
+++ OrchestrationExample/Services/LastStep.cs
@@ -0,0 +1,14 @@
+﻿using WorkflowCore.Interface;
+using WorkflowCore.Models;
+
+namespace OrchestrationExample.Services
+{
+    public class LastStep : StepBody
+    {
+        public override ExecutionResult Run(IStepExecutionContext context)
+        {
+            Console.WriteLine("Microservice B");
+            return ExecutionResult.Next();
+        }
+    }
+}
