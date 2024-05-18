4adbbf63248ed02d5e754e31536bd0fa631d26b9
Andrew Letskalyuk <andrewletskalyuk@gmail.com>
2024-05-18 22:17:19 +0300

    redis second step

--- RedisMQ.Practice/Program.cs
+++ RedisMQ.Practice/Program.cs
@@ -1,36 +1,44 @@
+using RedisMQ.Practice.Services;
+using StackExchange.Redis;
 
-namespace RedisMQ.Practice
+namespace RedisMQ.Practice;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        builder.Services
+            .AddSingleton<IConnectionMultiplexer>
+                (ConnectionMultiplexer.Connect
+                (builder.Configuration.GetConnectionString("Redis")));
+
+        builder.Services.AddSingleton<RedisService>();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
--- RedisMQ.Practice/Services/RedisService.cs
+++ RedisMQ.Practice/Services/RedisService.cs
@@ -1,6 +1,25 @@
-﻿namespace RedisMQ.Practice.Services
+﻿using StackExchange.Redis;
+
+namespace RedisMQ.Practice.Services;
+
+public class RedisService
 {
-    public class RedisService
+    private readonly IConnectionMultiplexer _redis;
+
+    public RedisService(IConnectionMultiplexer redis)
     {
+        _redis = redis;
+    }
+
+    public async Task<string> GetValueAsync(string key)
+    {
+        var db = _redis.GetDatabase();
+        return await db.StringGetAsync(key);
+    }
+
+    public async Task SetValueAsync(string key, string value)
+    {
+        var db = _redis.GetDatabase();
+        await db.StringSetAsync(key, value);
     }
 }
