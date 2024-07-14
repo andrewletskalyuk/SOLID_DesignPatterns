34f022c3412d00c978b023da0de47ccf39f34ef9
Andrew Letskalyuk <andrewletskalyuk@gmail.com>
2024-07-14 21:22:11 +0300

    kafka

---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace KafkaProducer;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var config = new ProducerConfig
+        {
+            BootstrapServers = "localhost:9092"
+        };
+
+        using(var producer = new ProducerBuilder<Null, string>(config).Build())
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var message = new Message<Null, string> { Value = $"Message{i}" };
+                producer.Produce("my-topic", message, (deliveryReport) =>
+                {
+                    if (deliveryReport.Error.Code != ErrorCode.NoError)
+                    {
+                        Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Produced message to {deliveryReport.TopicPartitionOffset}");
+                    }
+                });
+            }
+
+            // waiting ...
+            producer.Flush(TimeSpan.FromSeconds(10));
+        }
+    }
+}
